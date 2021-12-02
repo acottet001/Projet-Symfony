fb1d993e54fcbcec12fd645f6ad9cbfcf22dd7eb
Andreas Cottet <acottet001@iutbayonne.univ-pau.fr>
2021-12-02 09:28:44 +0100

    Création des pages par reponse brut sauf stages

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -6,15 +6,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProStageController extends AbstractController
+class ProstageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="prostage")
      */
     public function index(): Response
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
+        return $this->render('prostage/index.html.twig', [
+            'controller_name' => 'Bienvenue sur la page d \' accueil de Prostages',
+            'titre'=> 'Bienvenue sur la page d \' accueil de Prostages'
+        ]);
+    }
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function indexEntreprises(): Response
+    {
+        return $this->render('prostage/index.html.twig', [
+            'controller_name' => 'Page des entreprises',
+            'titre' => 'Cette page affichera la liste des entreprises proposant un stage'
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function indexFormations(): Response
+    {
+        return $this->render('prostage/index.html.twig', [
+            'controller_name' => 'Page de formations',
+            'titre' => 'Cette page affichera la liste des formations de l\'IUT',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/{id}", name="stages")
+     */
+    public function indexStages(): Response
+    {
+        return $this->render('prostage/index.html.twig', [
+            'controller_name' => 'Page de Stages',
+            'titre' => 'Cette page affichera le descriptif du stage ayant pour identifiant '
         ]);
     }
 }
