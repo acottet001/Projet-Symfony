435a6066bd236d477e035fe9527e42505e94710d
Andreas Cottet <acottet001@iutbayonne.univ-pau.fr>
2021-12-16 12:24:28 +0100

    Ajout de quelques vues

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -6,17 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProstageController extends AbstractController
+class ProStageController extends AbstractController
 {
     /**
      * @Route("/", name="prostage")
      */
     public function index(): Response
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'Bienvenue sur la page d \' accueil de Prostages',
-            'titre'=> 'Bienvenue sur la page d \' accueil de Prostages'
-        ]);
+        return $this->render('prostage/index.html.twig', []);
     }
 
     /**
@@ -24,10 +21,7 @@ class ProstageController extends AbstractController
      */
     public function indexEntreprises(): Response
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'Page des entreprises',
-            'titre' => 'Cette page affichera la liste des entreprises proposant un stage'
-        ]);
+        return $this->render('prostage/entreprises.html.twig', [ ]);
     }
 
     /**
@@ -35,20 +29,15 @@ class ProstageController extends AbstractController
      */
     public function indexFormations(): Response
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'Page de formations',
-            'titre' => 'Cette page affichera la liste des formations de l\'IUT',
-        ]);
+        return $this->render('prostage/formations.html.twig', []);
     }
 
     /**
      * @Route("/stages/{id}", name="stages")
      */
-    public function indexStages(): Response
+    public function indexStages($id=""): Response
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'Page de Stages',
-            'titre' => 'Cette page affichera le descriptif du stage ayant pour identifiant '
-        ]);
+        return $this->render('prostage/stages.html.twig', 
+        ['idStage' => $id]);
     }
 }
