2d4c02dec28ec06677650d0962ee90a81bc646c1
Andreas Cottet <acottet001@iutbayonne.univ-pau.fr>
2022-01-05 18:27:34 +0100

    Ajout de Faker + début d'envoi de données de test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,13 +12,32 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = \Faker\Factory::create('fr_FR');
+
         $stage = new Stage();
         $entreprise = new Entreprise();
         $formation = new Formation();
 
+        $entreprise->setNom($faker->company);
+        echo "test:";
+        // var_dump($faker->catchPhrase);
+        //Faker->bs et ->catchPhrase ne renvoient rien
+        $entreprise->setActivite("Informatique");
+        $entreprise->setAdresse($faker->address);
+
         $formation->setNomLong("DUT Informatique");
         $formation->setNomCourt("DUT Infor");
+
+        $stage->setTitre($faker->jobTitle);
+        $stage->setDomaine($faker->);
+        $stage->setEmail($faker->email);
+
+        $stage->setEntrepriseId($faker->);
+        $stage->setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
+
+
         $manager->persist($formation);
+        $manager->persist($entreprise);
 
         $manager->flush();
     }
