2f9543fa7e64aff4b314deb10c8bd211321cc470
Andreas Cottet <acottet001@iutbayonne.univ-pau.fr>
2022-01-14 00:20:45 +0100

    Amélioration de la génération des données

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,14 +12,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // Variable contenant le générateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
 
+        // La liste de toutes les entreprises générées
         $listeEntreprises = array();
+
+        // La liste de toutes les formations générées
         $listeFormations = array();
+
+        // Nombre d'entreprise à générer
         $nbEntreprises = 10;
-        $listeEntreprises = array();
+
+        // Nombre de stages à générer
         $nbStages = 10;
 
+        // Liste d'activités possible d'une entreprise
         $activiteEntrepriseDisponibles = array(
             "Logistique",
             "Commerçant",
@@ -32,6 +40,7 @@ class AppFixtures extends Fixture
             "Autres"
         );
     
+        // Liste des formations [Nom Court,Nom Long]
         $nomFormationsDisponibles = [
             ["DUT Tech de CO" , "DUT Techniques de Commercialisation"],
             ["Licence Pro COM Agrodistri et Agroali" , "Licence Professionnelle Commercialisation Agrodistribution et Agroalimentaire "],
@@ -44,32 +53,52 @@ class AppFixtures extends Fixture
             ["LP Eco","Licence Professionnelle Economie"]
         ];
 
+        // Ajout de toutes les formations dans Doctrine
         foreach($nomFormationsDisponibles as $nomFormationDisponible)
         {
+            // On crée une nouvelle formation
             $formation = new Formation();
+
+            // On lui set son nomCourt puis son nomLong
             $formation->setNomCourt($nomFormationDisponible[0]);
             $formation->setNomLong($nomFormationDisponible[1]);
+
+            // On ajoute la formation dans les données dans Doctrine
             $manager->persist($formation);
+
+            // On ajoute la formation dans la liste de toutes les formations
             $listeFormations[] = $formation;
         }
 
-
+        // Ajout de nombre d'entreprises dans les données
         for($i = 0; $i < $nbEntreprises; $i++)
         {
             //Faker->bs ne renvoient rien
+
+            // On crée une nouvelle entreprise
             $entreprise = new Entreprise();
+
+            // On lui set son nom, son actvité, son adresse
             $entreprise->setNom($faker->company);
-            $numActivite = $faker->numberBetween(0,8);
             $entreprise->setActivite($faker->randomElement($activiteEntrepriseDisponibles));
             $entreprise->setAdresse($faker->address);
+
             //$entreprise->setUrlSiteWeb($faker->url);
+            
+            // On ajoute la formation dans les données dans Doctrine
             $manager->persist($entreprise);
+
+            // On ajoute la formation dans la liste de toutes les formations
             $listeEntreprises[] = $entreprise;
         }
 
         for($i = 0; $i < $nbStages; $i++)
         {
+            // On crée une nouvelle entreprise
             $stage = new Stage();
+
+            // On lui set un titre de travail, un domaine(une activite)
+            // un email, une description et une entreprise et des formations
             $stage->setTitre($faker->jobTitle);
             $stage->setDomaine($faker->randomElement($activiteEntrepriseDisponibles));
             $stage->setEmail($faker->email);
@@ -77,9 +106,13 @@ class AppFixtures extends Fixture
 
             $stageEntreprise = $faker->randomElement($listeEntreprises);
             
+            // On ajoute le stage à l'entreprise
             $stageEntreprise->addStage($stage);
+
+            // On modifie l'entreprise dans les données dans Doctrine
             $manager->persist($stageEntreprise);
 
+            //
             $nbrFormations = $faker->numberBetween(0,2);
 
             foreach($faker->randomElements($listeFormations, $nbrFormations + 1) as $formation)
